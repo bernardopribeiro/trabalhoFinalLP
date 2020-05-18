fc3882667dcd96839b1f90a1e0564cb38504b4c3
160173019 <bernardopribeiro@hotmail.com>
2020-05-18 16:22:41 +0100

    Inserção das opções

--- Trabalho Final LP 2020/src/Main.java
+++ Trabalho Final LP 2020/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Main {
 
@@ -8,7 +9,24 @@ public class Main {
 	}
 
 	private static void menu() {
-		System.out.println("MENU");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("===== MENU =====");
+		System.out.println("1 -> Adicionar elemento a familia");
+		System.out.println("2 -> Elementos na familia");
+		System.out.println("3 -> Idade do Membro mais Velho");
+		System.out.println("4 -> Idade do Membro mais novo");
+		System.out.println("5 -> Gerações da Família");
+		System.out.println("6 -> Quantos membros por geração");
+		System.out.println("7 -> Listagem até ao grau desejado");
+		System.out.println("8 -> Listagem do grau desejado da familia");
+		System.out.println("9 -> Pré ordem da Família:");
+		System.out.println("10 -> Em ordem da Família:");
+		System.out.println("11 -> Pos Ordem da Família:");
+		System.out.println("12 -> Listar nível da ordem da família");
+		System.out.println("13 -> Remover Familiar");
+		System.out.println("0 -> Sair");
+		System.out.print("Escreva aqui a opção: ");
+		int choice = scanner.nextInt();
 	}
 
 }
