9f8435d4db083c28a75936b8ef5054f88d3d2cfa
160173019 <bernardopribeiro@hotmail.com>
2020-05-18 16:21:24 +0100

    Criação da Classe main, onde iremos ter o menu de utilizador

---
+++ Trabalho Final LP 2020/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		menu();
+
+	}
+
+	private static void menu() {
+		System.out.println("MENU");
+	}
+
+}
