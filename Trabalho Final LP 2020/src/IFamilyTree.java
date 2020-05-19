699bce65852b2349d0c6cd303be7b32f94debb21
160173019 <bernardopribeiro@hotmail.com>
2020-05-19 14:02:08 +0100

    Criamos a interface da classe FamilyTree para ir buscar os métodos da classe FamilyTree para mostrar na Main.

---
+++ Trabalho Final LP 2020/src/IFamilyTree.java
@@ -0,0 +1,20 @@
+
+/*
+	Tema de Trabalho: Arvore Geneologica
+
+	Elementos do grupo: 
+	-> Bernardo Pereira
+	-> Bernardo Barbosa
+ */
+
+public interface IFamilyTree {
+
+	public int numeroElementos();
+
+	public int maisVelho();
+
+	public int maisNovo();
+
+	public int alturaArvore();
+
+}
